1a25ea9c4de3c077fafa5c23f32247935ce6b563
wilsonibekason <wanbekgithub@gmail.com>
2022-07-09 05:44:40 -0700

    complete codewar repo file

---
+++ codewars/codewar1.js
@@ -0,0 +1,7 @@
+// initialising codewar 
+// question:  
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+const solution = (str, ending) => {
+    
+}
