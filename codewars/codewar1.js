82bada57f46dc3d579eb5bc6c5190cafcd4384bc
wilsonibekason <wanbekgithub@gmail.com>
2022-07-09 12:58:17 -0700

    complete create codewar one solution

--- codewars/codewar1.js
+++ codewars/codewar1.js
@@ -3,5 +3,12 @@
 // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
 const solution = (str, ending) => {
-    
+         str.indexOf(ending, str.length + ending.length ) !== -1;
 }
+
+const displayResults = solution('adc', 'bbc');
+ console.log(displayResults);
+
+ // link for donation to the 11 years old developer
+
+    // bit.ly/gdgaba
