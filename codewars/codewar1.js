e828dceb38b4dff4b2b5e158f57f7b27f9cba81f
wilsonibekason <wanbekgithub@gmail.com>
2022-07-09 17:13:45 -0700

    add two more solutions for the compare two strings

--- codewars/codewar1.js
+++ codewars/codewar1.js
@@ -3,7 +3,7 @@
 // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
 const solution = (str, ending) => {
-         str.indexOf(ending, str.length + ending.length ) !== -1;
+      return  str.indexOf(ending, str.length + ending.length ) !== -1;
 }
 
 const displayResults = solution('adc', 'bbc');
@@ -16,3 +16,20 @@ const displayResults = solution('adc', 'bbc');
     let stringTwo = 'abc';
     let generatedResult = stringOne.indexOf(stringTwo, stringOne.length + stringTwo.length) !== -1;
     console.log(generatedResult);
+
+    // solution2
+    const solution2 = (startString, endString) => {
+      return startString.endsWith(endString);
+    }
+
+    console.log(solution2('xxs', 'bbc'));
+
+    // solution3 for codewar algorithm
+    const solution3 = (startString, endString) => {
+      // return startString.endsWith(endString);
+      return startString.substr(-endString.length) === -endString
+    }
+
+    console.log(solution3('xxs', 'bbc'));
+
+    console.log(solution)
